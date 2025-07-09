10bbbed76be9f54c38e7640a5d693145d19c7114
Antonyhyson <work.antonyhyson@gmail.com>
2025-07-10 00:05:27 +0100

    feat: setup github pages deployment

--- setup-db.js
+++ setup-db.js
@@ -1,10 +1,6 @@
-import { sql } from './lib/db.js';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const { sql } = require('./lib/db');
+const fs = require('fs');
+const path = require('path');
 
 async function setupDatabase() {
   try {
